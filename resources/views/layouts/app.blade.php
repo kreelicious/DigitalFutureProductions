21f463c814bce161bb1e8cc37390c411eb138b53
Lyn Williams <lyn@kreelicious.com>
2026-02-26 16:23:47 +0000

    Guard Vite asset loading when manifest is missing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @php
+        $viteHotFile = public_path('hot');
+        $viteManifestFile = public_path('build/manifest.json');
+    @endphp
+
+    @if (file_exists($viteHotFile) || file_exists($viteManifestFile))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 <body>
     @include('partials.nav')
