c22b75a677de7e1655433bb6a396564cc831c64d
Lyn Williams <lyn@kreelicious.com>
2026-02-26 15:37:02 +0000

    Handle missing Vite manifest in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ ($pageTitle ?? 'Future Digital Productions') . ' | Future Digital Productions' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @php
+        $viteManifest = public_path('build/manifest.json');
+        $viteHotFile = public_path('hot');
+    @endphp
+
+    @if (file_exists($viteManifest) || file_exists($viteHotFile))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 <body>
     @include('partials.nav')
